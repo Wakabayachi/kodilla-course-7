b4fdc309c91959550edde7aa9e5821f7f64ac43a
Daniel Nastalski <maspa1987@gmail.com>
2024-05-16 22:54:03 +0200

    first commit

---
+++ src/main/java/com/kodilla/kodillacourse7/KodillaCourse7Application.java
@@ -0,0 +1,13 @@
+package com.kodilla.kodillacourse7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaCourse7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaCourse7Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/kodilla/kodillacourse7/KodillaCourse7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.kodillacourse7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodillaCourse7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
